b2b5cf4536d30cc8822a2c8fe53e76267647a147
theAxels <angkianto.laurence@binus.ac.id>
2023-03-29 14:42:17 +0700

    initial commit

--- HCI-LAB/js/characters.js
+++ HCI-LAB/js/characters.js
@@ -25,7 +25,7 @@ card1.onclick = function(){
     pic[1].classList.add("ON")
     document.getElementById("after").style.left = "20%"
     document.getElementById("name").innerText = "JEAN"
-    document.getElementById("desc").innerText = "As the Acting Grand Master of the Knights, Jean has always been devoted to her duties and maintaining peace in Mondstadt. She had taken precautions long before the onset of Stormterror's assault, and she will guard Mondstadt with her life as always."
+    document.querySelector("#desc p").innerText = "As the Acting Grand Master of the Knights, Jean has always been devoted to her duties and maintaining peace in Mondstadt. She had taken precautions long before the onset of Stormterror's assault, and she will guard Mondstadt with her life as always."
 }
 
 
@@ -38,7 +38,7 @@ card2.onclick = function(){
     document.getElementById("after").style.left = "0"
     pic[2].classList.add("ON")
     document.getElementById("name").innerText = "MONA"
-    document.getElementById("desc").innerText = "A mysterious young astrologer who proclaims herself to be 'Astrologist Mona Megistus,' and who possesses abilities to match the title. Erudite, but prideful. Though she is often strapped for cash and lives a life of thrift, she is resolved to never use astrology for profit... It is this very resolution that has caused her to constantly fret about money."
+    document.querySelector("#desc p").innerText = "A mysterious young astrologer who proclaims herself to be 'Astrologist Mona Megistus,' and who possesses abilities to match the title. Erudite, but prideful. Though she is often strapped for cash and lives a life of thrift, she is resolved to never use astrology for profit... It is this very resolution that has caused her to constantly fret about money."
 }
 
 card3.onclick = function(){
@@ -50,7 +50,7 @@ card3.onclick = function(){
     document.getElementById("current").style.left = "20%"
     document.getElementById("after").style.left = "-20%"
     document.getElementById("name").innerText = "EULA"
-    document.getElementById("desc").innerText = "A rebellious descendant of the old aristocracy who is always out on the battlefield.As one born into the old aristocracy, carrying the bloodline of sinners, Eula has needed a unique approach to the world to navigate the towering walls of prejudice peacefully. Of course, this did not prevent her from severing ties with her clan. As the outstanding Spindrift Knight, she hunts down Mondstadt's enemies in the wild to exact her unique 'vengeance.'"
+    document.querySelector("#desc p").innerText = "A rebellious #descendant of the old aristocracy who is always out on the battlefield.As one born into the old aristocracy, carrying the bloodline of sinners, Eula has needed a unique approach to the world to navigate the towering walls of prejudice peacefully. Of course, this did not prevent her from severing ties with her clan. As the outstanding Spindrift Knight, she hunts down Mondstadt's enemies in the wild to exact her unique 'vengeance.'"
 }
 
 //Liyue
@@ -69,7 +69,7 @@ card21.onclick = function(){
     pic1[1].classList.add("ON")
     document.getElementById("after2").style.left = "20%"
     document.getElementById("name2").innerText = "HUTAO"
-    document.getElementById("desc2").innerText = "Hu Tao is the 77th Director of the Wangsheng Funeral Parlor, a person vital to managing Liyue's funerary affairs. She does her utmost to flawlessly carry out a person's last rites and preserve the world's balance of yin and yang."
+    document.querySelector("#desc2 p").innerText = "Hu Tao is the 77th Director of the Wangsheng Funeral Parlor, a person vital to managing Liyue's funerary affairs. She does her utmost to flawlessly carry out a person's last rites and preserve the world's balance of yin and yang."
 }
 
 
@@ -82,7 +82,7 @@ card22.onclick = function(){
     document.getElementById("after2").style.left = "0"
     pic1[2].classList.add("ON")
     document.getElementById("name2").innerText = "KEQING"
-    document.getElementById("desc2").innerText = "The Yuheng of the Liyue Qixing. Keqing has much to say about Rex Lapis' unilateral approach to policymaking in Liyue ⁠— but in truth, gods admire skeptics such as her quite a lot. She firmly believes that humanity's future should be determined by humans themselves, and that they can even do better than the archons and adepti have done for them. In order to prove this, she works harder than anyone else."
+    document.querySelector("#desc2 p").innerText = "The Yuheng of the Liyue Qixing. Keqing has much to say about Rex Lapis' unilateral approach to policymaking in Liyue ⁠— but in truth, gods admire skeptics such as her quite a lot. She firmly believes that humanity's future should be determined by humans themselves, and that they can even do better than the archons and adepti have done for them. In order to prove this, she works harder than anyone else."
 }
 
 card23.onclick = function(){
@@ -94,7 +94,7 @@ card23.onclick = function(){
     document.getElementById("current2").style.left = "20%"
     document.getElementById("after2").style.left = "-20%"
     document.getElementById("name2").innerText = "BEIDOU"
-    document.getElementById("desc2").innerText = "Captain of the Crux, with quite the reputation in Liyue.There are those who say she can split mountains and part the sea. Others say she draws lightning through her sword. Some say that even the mightiest of sea beasts are no match for her. For those not from Liyue, it may sound like a hearty joke, but those that have sailed with her will say— 'No matter what sea beasts there may be, Beidou will be sure to split them all in two.'"
+    document.querySelector("#desc2 p").innerText = "Captain of the Crux, with quite the reputation in Liyue.There are those who say she can split mountains and part the sea. Others say she draws lightning through her sword. Some say that even the mightiest of sea beasts are no match for her. For those not from Liyue, it may sound like a hearty joke, but those that have sailed with her will say— 'No matter what sea beasts there may be, Beidou will be sure to split them all in two.'"
 }
 
 //Inazuma
@@ -113,7 +113,7 @@ card31.onclick = function(){
     pic2[1].classList.add("ON")
     document.getElementById("after3").style.left = "20%"
     document.getElementById("name3").innerText = "KAMISATO AYAKA"
-    document.getElementById("desc3").innerText = "Daughter of the Yashiro Commission's Kamisato Clan from Inazuma. Dignified and elegant, wise and determined. Sincere and pleasant to others. Universally loved by the Inazuma people, she has earned the title of Shirasagi Himegimi."
+    document.querySelector("#desc3 p").innerText = "Daughter of the Yashiro Commission's Kamisato Clan from Inazuma. Dignified and elegant, wise and determined. Sincere and pleasant to others. Universally loved by the Inazuma people, she has earned the title of Shirasagi Himegimi."
 }
 
 
@@ -126,7 +126,7 @@ card32.onclick = function(){
     document.getElementById("after3").style.left = "0"
     pic2[2].classList.add("ON")
     document.getElementById("name3").innerText = "RAIDEN SHOGUN"
-    document.getElementById("desc3").innerText = "The Raiden Shogun is the awesome and terrible power of thunder incarnate, the exalted ruler of the Inazuma Shogunate. With the might of lightning at her disposal, she commits herself to the solitary pursuit of eternity."
+    document.querySelector("#desc3 p").innerText = "The Raiden Shogun is the awesome and terrible power of thunder incarnate, the exalted ruler of the Inazuma Shogunate. With the might of lightning at her disposal, she commits herself to the solitary pursuit of eternity."
 }
 
 card33.onclick = function(){
@@ -139,7 +139,7 @@ card33.onclick = function(){
     document.getElementById("after3").style.left = "-20%"
     
     document.getElementById("name3").innerText = "YOIMIYA"
-    document.getElementById("desc3").innerText = "A talented pyrotechnician. The current owner of Naganohara Fireworks known as the 'Queen of the Summer Festival.' A girl filled with fiery passion. The uncompromising childish innocence and the obsession with craftsmanship intertwine in her to create a spectacular blaze."
+    document.querySelector("#desc3 p").innerText = "A talented pyrotechnician. The current owner of Naganohara Fireworks known as the 'Queen of the Summer Festival.' A girl filled with fiery passion. The uncompromising childish innocence and the obsession with craftsmanship intertwine in her to create a spectacular blaze."
 }
 
 //Sumeru
@@ -158,7 +158,7 @@ card41.onclick = function(){
     pic3[1].classList.add("ON")
     document.getElementById("after4").style.left = "20%"
     document.getElementById("name4").innerText = "CANDACE"
-    document.getElementById("desc4").innerText = "The guardian of Aaru Village who is gentle and benevolent. She will not allow anyone to harm Aaru Village under her watch."
+    document.querySelector("#desc4 p").innerText = "The guardian of Aaru Village who is gentle and benevolent. She will not allow anyone to harm Aaru Village under her watch."
 }
 
 
@@ -171,7 +171,7 @@ card42.onclick = function(){
     document.getElementById("after4").style.left = "0"
     pic3[2].classList.add("ON")
     document.getElementById("name4").innerText = "NAHIDA"
-    document.getElementById("desc4").innerText = "Lesser Lord Kusanali dwells deep in the Sanctuary of Surasthana, and has never really been in the limelight, nor has she even been mentioned much. Her burden is heavy, but though she may experience loneliness, and though darkness is all she sees before her, she will not stop moving forward."
+    document.querySelector("#desc4 p").innerText = "Lesser Lord Kusanali dwells deep in the Sanctuary of Surasthana, and has never really been in the limelight, nor has she even been mentioned much. Her burden is heavy, but though she may experience loneliness, and though darkness is all she sees before her, she will not stop moving forward."
 }
 
 card43.onclick = function(){
@@ -183,7 +183,7 @@ card43.onclick = function(){
     document.getElementById("current4").style.left = "20%"
     document.getElementById("after4").style.left = "-20%"
     document.getElementById("name4").innerText = "DEHYA"
-    document.getElementById("desc4").innerText = "A member of'The Eremites' a loosely-organized mercenary organization. She is brave, powerful, and enjoys an excellent reputation among mercenaries."
+    document.querySelector("#desc4 p").innerText = "A member of'The Eremites' a loosely-organized mercenary organization. She is brave, powerful, and enjoys an excellent reputation among mercenaries."
 }
 
 //Slider
